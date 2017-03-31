ff913e6cc16d2bbfe7fc19aa0726da2e49f6e90b
Henry <henryhobhouse@gmail.com>
2017-03-31 17:01:30 +0100

    updated so that students are displayed using array

--- directory.rb
+++ directory.rb
@@ -1,19 +1,21 @@
-student_count = 11
-
-# the list of students
+# the list of students in an array
+students = [
+  "Dr Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Nomran Bates",
+]
+# and then prints them
 puts "The students of Villians Academy"
-puts "--------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts "-----------"
+students.each{ |student| puts student }
 
 # displaying the number of students
-print "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
