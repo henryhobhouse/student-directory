b7e741ca28d1d0ca3b96151a98a819bc493fe35f
Henry <henryhobhouse@gmail.com>
2017-04-01 09:20:54 +0100

    Updated directory to allow user input of names

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 # the list of students in an array
+=begin
 students = [
   {name: "Dr Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -12,13 +13,35 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november},
 ]
+=end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  print "> "
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    print "> "
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
 end
 
-def print(names)
+def print_name(names)
   names.each{ |i| puts "#{i[:name]} (#{i[:cohort]} cohort)" }
 end
 
@@ -27,6 +50,7 @@ def print_footer (names)
 end
 
 #nothing happens until we call the methods
+students = input_students
 print_header
-print(students)
+print_name(students)
 print_footer(students)
